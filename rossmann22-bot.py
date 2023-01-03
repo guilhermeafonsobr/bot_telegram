285971c4b78de840b51f4b54b7be835883bb028f
guilhermeafonsobr <guilhermeafonso2503@hotmail.com>
2023-01-03 19:22:22 -0300

    Identacao resolvida

--- rossmann22-bot.py
+++ rossmann22-bot.py
@@ -84,38 +84,38 @@ def parse_message(message):
 app= Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-    def index(): 
-        if request.method == 'POST': 
-        message = request.get_json()
+def index(): 
+    if request.method == 'POST': 
+    message = request.get_json()
 
-        chat_id, store_id = parse_message(message)
+    chat_id, store_id = parse_message(message)
     
-            if store_id != 'error': 
+        if store_id != 'error': 
         #loading data
-            data=load_dataset(store_id)
+        data=load_dataset(store_id)
         
-                if data != 'error':
+            if data != 'error':
             #prediction 
-                d1=predict(data)
+            d1=predict(data)
 
             #calculation 
-                d2 = d1[['store', 'prediction']].groupby('store').sum().reset_index()
+            d2 = d1[['store', 'prediction']].groupby('store').sum().reset_index()
             #send message
-                msg = 'Store number {} will sell R${:,.2f} in the next 6 weeks'.format(
-                d2.['store'].values[0],
-                d2.['prediction'].values[0])
+            msg = 'Store number {} will sell R${:,.2f} in the next 6 weeks'.format(
+            d2.['store'].values[0],
+            d2.['prediction'].values[0])
 
-                send_message(chat_id, msg) 
-                return Response('Ok', status=200)
+            send_message(chat_id, msg) 
+            return Response('Ok', status=200)
             #send message
             else:
             send_message(chat_id, 'Store not available')
             return Response('Ok', status=200)
-            else: 
+        else: 
             send_message(chat_id, 'Store ID is Wrong') 
-            return Response ('OK', status=200) 
-        else:
-        return '<h1> Rossmann Telegram BOT <h1>'
+        return Response ('OK', status=200) 
+    else:
+    return '<h1> Rossmann Telegram BOT <h1>'
 
 if __name__='__main__':
     port = os.environ.get('PORT', 5000)
